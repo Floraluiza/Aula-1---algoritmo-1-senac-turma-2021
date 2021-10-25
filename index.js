bb6ca3898585bd5d6593c5f46f7ae873a980631f
Flora Santana <39812840+Floraluiza@users.noreply.github.com>
2021-10-25 00:55:19 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var remetente 
+var destinatario
+
+remetente = prompt('Digite seu remetente: ')
+destinatario = prompt('Digite seu destinatario: ')
+
+
+//alert('Meu primeiro Programa')
+//console.log('Hello World')
+
+alert('Remetente recebido: ' + remetente)
+
+alert('Destinatario recebido: ' + destinatario)
+
+console.log('Gosto muito de vocês!')
